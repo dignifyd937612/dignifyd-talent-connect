43c151423040614dd83619e552fe7ced2a974cb2
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-06-05 18:09:12 +0530

    logo link added

--- src/data/logos.js
+++ src/data/logos.js
@@ -1,81 +1,101 @@
-import maybelline from "../assets/images/home/logos/maybelline.png";
-import lotus from "../assets/images/home/logos/lotus.png";
-import boat from "../assets/images/home/logos/boat.png";
-import oppo from "../assets/images/home/logos/oppo.png";
-import minimalist from "../assets/images/home/logos/minimalist.png";
-import avvalar from "../assets/images/home/logos/avvalar.png";
 import bajaj_finserv from "../assets/images/home/logos/bajaj_finserv.png";
-import pci_INfra from "../assets/images/home/logos/PCI_infra.png";
-import roca from "../assets/images/home/logos/roca.png";
-import ICCPL from "../assets/images/home/logos/iccpl.png";
-import mamaearth from "../assets/images/home/logos/mamaearth.png";
-import cordelia from "../assets/images/home/logos/cordelia.png";
-import livspace from "../assets/images/home/logos/livspace.png";
-import garnier from "../assets/images/home/logos/garnier.png";
-import kshm from "../assets/images/home/logos/kshm.png";
-import giva from "../assets/images/home/logos/giva.png";
-import dot_key from "../assets/images/home/logos/dot_key.png";
-import cerave from "../assets/images/home/logos/cerave.png";
-import aqualogica from "../assets/images/home/logos/aqualogica.png";
-import snitch from "../assets/images/home/logos/snitch.png";
 import aminu from "../assets/images/home/logos/aminu.png";
-import techchef from "../assets/images/home/logos/techchef.png";
-import indeed from "../assets/images/aboutUs/logos/indeed.png";
-import naukri from "../assets/images/aboutUs/logos/naukri.png";
-import monster from "../assets/images/aboutUs/logos/monster.png";
-import nivea from "../assets/images/aboutUs/logos/nivea.png";
-import office365 from "../assets/images/aboutUs/logos/office365.png";
-import perplexity from "../assets/images/aboutUs/logos/perplexity.png";
-import salesforce from "../assets/images/aboutUs/logos/salesforce.png";
-import zoho from "../assets/images/aboutUs/logos/zoho.png";
+import star_link from "../assets/images/home/logos/starlink_qatar.png";
+import IGT from "../assets/images/home/logos/IGT.png";
+import calsoft from "../assets/images/home/logos/calsoft.png";
+import bajaj_tech from "../assets/images/home/logos/bajaj_tech.png";
+import material from "../assets/images/home/logos/material+.png";
+import compunel from "../assets/images/home/logos/compunel.png";
+import amerprise from "../assets/images/home/logos/amerprise.png";
+import rbc_bank from "../assets/images/home/logos/rbc.png";
+import ibm from "../assets/images/home/logos/IBM.png";
+import bank_of_montreal from "../assets/images/home/logos/BMO.png";
+import Hipl from "../assets/images/home/logos/HIPL.png";
+import SOC from "../assets/images/home/logos/state_of_california.png";
+import SOT from "../assets/images/home/logos/state_of_texas.png";
+import combay_solutions from "../assets/images/home/logos/kombay_soltiuon.png";
+import Kpmg from "../assets/images/home/logos/kpmg.png";
+import indigo from "../assets/images/home/logos/indigo.png";
+import imz from "../assets/images/home/logos/IMZ.png";
+import radical_minds from "../assets/images/home/logos/radical _minds.png";
+import collabera from "../assets/images/home/logos/collabera.png";
+import cognizant from "../assets/images/home/logos/cognizant.png";
+import radius from "../assets/images/home/logos/radius.png";
 
 export const collaborations = [
-  maybelline,
-  lotus,
-  boat,
-  oppo,
-  minimalist,
-  avvalar,
+  star_link,
   bajaj_finserv,
-  pci_INfra,
-  roca,
-  ICCPL,
-  mamaearth,
-  cordelia,
-  livspace,
-  garnier,
-  kshm,
-  giva,
-  dot_key,
-  cerave,
-  aqualogica,
-  snitch,
   aminu,
-  techchef,
+  IGT,
+  calsoft,
+  bajaj_tech,
+  material,
+  compunel,
+  collabera,
+  amerprise,
+  rbc_bank,
+  ibm,
+  cognizant,
+  bank_of_montreal,
+  Hipl,
+  SOC,
+  SOT,
+  combay_solutions,
+  Kpmg,
+  indigo,
+  imz,
+  radical_minds,
+  radius,
 ];
 
 export const partnersLogos = [
-  maybelline,
-  lotus,
-  boat,
-  oppo,
-  minimalist,
-  avvalar,
+  star_link,
   bajaj_finserv,
-  pci_INfra,
-  roca,
-  ICCPL,
-  mamaearth,
-  cordelia,
+  aminu,
+  IGT,
+  calsoft,
+  bajaj_tech,
+  material,
+  compunel,
+  collabera,
+  amerprise,
+  rbc_bank,
+  ibm,
+  cognizant,
+  bank_of_montreal,
+  Hipl,
+  SOC,
+  SOT,
+  combay_solutions,
+  Kpmg,
+  indigo,
+  imz,
+  radical_minds,
+  radius,
 ];
 
 export const techSupportLogos = [
-  indeed,
-  naukri,
-  monster,
-  nivea,
-  office365,
-  perplexity,
-  salesforce,
-  zoho,
+  star_link,
+  bajaj_finserv,
+  aminu,
+  IGT,
+  calsoft,
+  bajaj_tech,
+  material,
+  compunel,
+  collabera,
+  amerprise,
+  rbc_bank,
+  ibm,
+  cognizant,
+  bank_of_montreal,
+  Hipl,
+  SOC,
+  SOT,
+  combay_solutions,
+  Kpmg,
+  indigo,
+  imz,
+  radical_minds,
+  radius,
 ];
