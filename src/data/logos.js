beda98c6506a850635a9c65e96a0138403083d37
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-06-06 12:57:04 +0530

    logo updated

--- src/data/logos.js
+++ src/data/logos.js
@@ -1,72 +1,71 @@
 import bajaj_finserv from "../assets/images/home/logos/bajaj_finserv.png";
 import aminu from "../assets/images/home/logos/aminu.png";
-import star_link from "../assets/images/home/logos/starlink_qatar.png";
-import IGT from "../assets/images/home/logos/igt.png";
+import starlink_qatar from "../assets/images/home/logos/starlink_qatar.png";
+import igt from "../assets/images/home/logos/igt.png";
 import calsoft from "../assets/images/home/logos/calsoft.png";
 import bajaj_tech from "../assets/images/home/logos/bajaj_tech.png";
-import material from "../assets/images/home/logos/material+.png";
+import material from "../assets/images/home/logos/material.png";
 import compunel from "../assets/images/home/logos/compunel.png";
 import amerprise from "../assets/images/home/logos/amerprise.png";
-import rbc_bank from "../assets/images/home/logos/rbc.png";
-import ibm from "../assets/images/home/logos/IBM.png";
-import bank_of_montreal from "../assets/images/home/logos/BMO.png";
-import Hipl from "../assets/images/home/logos/HIPL.png";
-import SOC from "../assets/images/home/logos/state_of_california.png";
-import SOT from "../assets/images/home/logos/state_of_texas.png";
-import combay_solutions from "../assets/images/home/logos/kombay_soltiuon.png";
+import rbc from "../assets/images/home/logos/rbc.png";
+import ibm from "../assets/images/home/logos/ibm.png";
+import bank_of_montreal from "../assets/images/home/logos/bank_of_montreal.png";
+import hipl from "../assets/images/home/logos/hipl.png";
+import state_of_california from "../assets/images/home/logos/state_of_california.png";
+import state_of_texas from "../assets/images/home/logos/state_of_texas.png";
+import combay_solutions from "../assets/images/home/logos/combay_solutions.png";
 import Kpmg from "../assets/images/home/logos/kpmg.png";
 import indigo from "../assets/images/home/logos/indigo.png";
-import imz from "../assets/images/home/logos/IMZ.png";
-import radical_minds from "../assets/images/home/logos/radical__minds.png";
+import imz from "../assets/images/home/logos/imz.png";
+import radical_minds from "../assets/images/home/logos/radical_minds.png";
 import collabera from "../assets/images/home/logos/collabera.png";
 import cognizant from "../assets/images/home/logos/cognizant.png";
 import radius from "../assets/images/home/logos/radius.png";
 
 export const collaborations = [
-  star_link,
+  starlink_qatar,
   bajaj_finserv,
   aminu,
-  IGT,
+  igt,
   calsoft,
   bajaj_tech,
   material,
   compunel,
   collabera,
   amerprise,
-  rbc_bank,
+  rbc,
   ibm,
   cognizant,
   bank_of_montreal,
-  Hipl,
-  SOC,
-  SOT,
+  hipl,
+  state_of_california,
+  state_of_texas,
   combay_solutions,
   Kpmg,
   indigo,
   imz,
   radical_minds,
   radius,
-  
 ];
 
 export const partnersLogos = [
-  star_link,
+  starlink_qatar,
   bajaj_finserv,
   aminu,
-  IGT,
+  igt,
   calsoft,
   bajaj_tech,
   material,
   compunel,
   collabera,
   amerprise,
-  rbc_bank,
+  rbc,
   ibm,
   cognizant,
   bank_of_montreal,
-  Hipl,
-  SOC,
-  SOT,
+  hipl,
+  state_of_california,
+  state_of_texas,
   combay_solutions,
   Kpmg,
   indigo,
@@ -76,23 +75,23 @@ export const partnersLogos = [
 ];
 
 export const techSupportLogos = [
-  star_link,
+  starlink_qatar,
   bajaj_finserv,
   aminu,
-  IGT,
+  igt,
   calsoft,
   bajaj_tech,
   material,
   compunel,
   collabera,
   amerprise,
-  rbc_bank,
+  rbc,
   ibm,
   cognizant,
   bank_of_montreal,
-  Hipl,
-  SOC,
-  SOT,
+  hipl,
+  state_of_california,
+  state_of_texas,
   combay_solutions,
   Kpmg,
   indigo,
