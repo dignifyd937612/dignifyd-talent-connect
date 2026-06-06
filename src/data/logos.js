33f2dea464ff7b15601f4702e2af1eca34f65a64
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-06-06 13:34:31 +0530

    add intense pvt ltd  logo

--- src/data/logos.js
+++ src/data/logos.js
@@ -20,6 +20,7 @@ import collabera from "../assets/images/home/logos/collabera.png";
 import cognizant from "../assets/images/home/logos/cognizant.png";
 import radius from "../assets/images/home/logos/radius.png";
 import imz_2 from "../assets/images/home/logos/imz_2.png";
+import intense from "../assets/images/home/logos/intense.png";
 
 export const collaborations = [
   starlink_qatar,
@@ -43,7 +44,7 @@ export const collaborations = [
   indigo,
   radical_minds,
   radius,
-  imz_2
+  imz_2,
 ];
 
 export const partnersLogos = [
@@ -68,7 +69,7 @@ export const partnersLogos = [
   indigo,
   radical_minds,
   radius,
-  imz_2
+  imz_2,
 ];
 
 export const techSupportLogos = [
@@ -93,5 +94,5 @@ export const techSupportLogos = [
   indigo,
   radical_minds,
   radius,
-  imz_2
+  imz_2,
 ];
