0e9606d1c56787999cdb468ec12d0568dad30ac5
sumit-dignifyd <sumit.singh@dignifyd.io>
2026-08-05 11:36:44 +0530

    feat: Refactor service worker for improved caching and add robots.txt for SEO

--- public/sw.js
+++ public/sw.js
@@ -1,18 +1,13 @@
 const CACHE_NAME = "talent-connect-v1";
 
-const urlsToCache = ["/"];
-
+// Install
 self.addEventListener("install", (event) => {
   console.log("Service Worker Installed");
 
-  event.waitUntil(
-    caches.open(CACHE_NAME).then((cache) => {
-      console.log("Caching App Shell");
-      return cache.addAll(urlsToCache);
-    }),
-  );
+  self.skipWaiting();
 });
 
+// Activate
 self.addEventListener("activate", (event) => {
   console.log("Service Worker Activated");
 
@@ -21,28 +16,49 @@ self.addEventListener("activate", (event) => {
       return Promise.all(
         cacheNames.map((cache) => {
           if (cache !== CACHE_NAME) {
-            console.log("Removing Old Cache:", cache);
+            console.log("Deleting:", cache);
             return caches.delete(cache);
           }
         }),
       );
     }),
   );
+
+  self.clients.claim();
 });
 
+// Fetch
 self.addEventListener("fetch", (event) => {
+  // Only GET requests will be cached
+  if (event.request.method !== "GET") {
+    return;
+  }
+
   event.respondWith(
     caches.match(event.request).then((cachedResponse) => {
+      // Cache Hit
       if (cachedResponse) {
+        console.log("📦 Cache:", event.request.url);
         return cachedResponse;
       }
 
+      // Cache Miss
       return fetch(event.request).then((networkResponse) => {
-        return caches.open(CACHE_NAME).then((cache) => {
-          cache.put(event.request, networkResponse.clone());
-
+        // If response is not valid, don't cache it
+        if (!networkResponse || networkResponse.status !== 200) {
           return networkResponse;
+        }
+
+        // Save response in cache
+        const responseClone = networkResponse.clone();
+
+        caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, responseClone);
         });
+
+        console.log("Network:", event.request.url);
+
+        return networkResponse;
       });
     }),
   );
